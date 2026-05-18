44333b3d0b80ccbe9bfbc3e579c53bdc00ba0c67
jaehyeok <revol3154@gmail.com>
2026-05-18 22:51:25 +0900

    [ feat ] : 1-Send, 버퍼에 쌓아 놓고, send 스레드에서 전송

--- includes/ClientInfo.h
+++ includes/ClientInfo.h
@@ -10,12 +10,9 @@
 struct stClientInfo
 {
 public:
-	stClientInfo()
-	{
-		ZeroMemory(&mRecvOverlappedEx, sizeof(stOverlappedEx));
-		mSock = INVALID_SOCKET;
-		ZeroMemory(mRecvBuf.get(), MAX_SOCKBUF);
-	}
+	stClientInfo();
+	
+	~stClientInfo() = default;
 
 	void Init(const UINT32 index);
 	
@@ -39,12 +36,21 @@ public:
 
 	bool SendMsg(const UINT32 dataSize_, shared_ptr<char[]> pMsg_);
 
+	bool SendIO();
+
 	void SendCompleted(const UINT32 dataSize_);
 
 private:
 	INT32 mIndex = { 0 };
 	SOCKET mSock;
 	stOverlappedEx mRecvOverlappedEx;
+	stOverlappedEx mSendOverlappedEx;
 
 	shared_ptr<char[MAX_SOCKBUF]> mRecvBuf = {};
+
+	mutex mSendLock;
+	bool mIsSending = false;
+	UINT64 mSendPos = 0;
+	char mSendBuf[MAX_SOCK_SENDBUF];
+	char mSendingBuf[MAX_SOCK_SENDBUF];
 };
